ae24b650a6dce6cd6d1978f141b66d8b6a06963a
Christopher Yihan Jiang <chrisyh.jiang@gmail.com>
2024-05-15 23:29:05 -0700

    new features, fingerprint, vault id, password editing, password show

---
+++ build/lib/password_manager/__init__.py
@@ -0,0 +1,1 @@
+# password_manager/__init__.py
--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ setup(
     name='password_manager',
     version='0.1',
     packages=find_packages(),
-    include_package_data=True,
     install_requires=[
         'click',
         'cryptography',
         'pam',
-        'pyobjc',
         'pyobjc-framework-Cocoa',
         'pyobjc-framework-LocalAuthentication',
+        'pyobjc-core',
     ],
     entry_points={
         'console_scripts': [
